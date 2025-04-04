8a328e92f9c1ff32cdfa4234220c617a1a746456
Janaina Barboza <barboza205@usf.edu>
2025-04-04 18:57:47 -0400

    first commit

---
+++ src/Components/Greeting.jsx
@@ -0,0 +1,12 @@
+//Task 1: Create a new Component
+
+function Greeting(){
+    return (
+    <div>
+        <h2>Hello from the Greeing Component!</h2>
+        <p>This is your first custom component in React.</p>
+    </div>
+    );
+}
+
+export default Greeting;
